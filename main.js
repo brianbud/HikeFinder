75bc2f2b718fb4a0d63d7892692300fa871321f1
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-03-01 21:51:21 -0500

    nav section on desktop width

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 const hamburgerEl = document.getElementById("hamburgerMenuIcon");
+const bodyEl = document.getElementsByTagName("body")[0];
 
 hamburgerEl.addEventListener("click", function () {
-  let linksEl = document.getElementById("myLinks");
-  if (linksEl.style.display === "block") {
-    linksEl.style.display = "none";
-  } else {
-    linksEl.style.display = "block";
+  if (bodyEl.offsetWidth < 700) {
+    let linksEl = document.getElementById("myLinks");
+    if (linksEl.style.display === "block") {
+      linksEl.style.display = "none";
+    } else {
+      linksEl.style.display = "block";
+    }
   }
 });
