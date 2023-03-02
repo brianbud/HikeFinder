066ec12de3adea7d6da4d2fc2b0756b9efc92ab0
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-03-01 19:09:43 -0500

    hamburger menu

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+const hamburgerEl = document.getElementById("hamburgerMenuIcon");
 
+hamburgerEl.addEventListener("click", function () {
+  let linksEl = document.getElementById("myLinks");
+  if (linksEl.style.display === "block") {
+    linksEl.style.display = "none";
+  } else {
+    linksEl.style.display = "block";
+  }
+});
